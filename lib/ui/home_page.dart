29585e0c160423a63be2d96cc73c37c00e85d687
Sergio Souza Novak <serginhosnovak@hotmail.com>
2019-07-29 17:15:06 -0300

    feito o esboço da ui

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -8,32 +8,71 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
-
   ContactHelper helper = ContactHelper();
 
+  List<Contact> listaDeContatos = List();
 
-
-  //Esse metodo é invocado quando a pagina for criada
   @override
   void initState() {
-    super.initState();
-    // Contact c = Contact();
-    // c.name = "name"; 
-    // c.email = "email"; 
-    // c.img = "img"; 
-    // c.phone = "phone"; 
-    // helper.saveContact(c);
-    helper.getAllContacts().then((list){
-        print(list);
-
-
+    helper.getAllContacts().then((list) {
+      setState(() {
+        listaDeContatos = list;
+      });
     });
 
-
+    super.initState();
   }
+
+  // //Esse metodo é invocado quando a pagina for criada
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   // Contact c = Contact();
+  //   // c.name = "name";
+  //   // c.email = "email";
+  //   // c.img = "img";
+  //   // c.phone = "phone";
+  //   // helper.saveContact(c);
+  //   helper.getAllContacts().then((list){
+  //       print(list);
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Contatos"),
+          backgroundColor: Colors.red,
+          centerTitle: true),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+          padding: EdgeInsets.all(10),
+          itemCount: listaDeContatos.length,
+          itemBuilder: (context, index) {}),
+    );
+  }
+
+  Widget ContactCard(BuildContext context, int index) {
+    return GestureDetector(
+        child: Card(
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: <Widget>[
+            Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(image: null))),
+          ],
+        ),
+      ),
+    ));
   }
 }
